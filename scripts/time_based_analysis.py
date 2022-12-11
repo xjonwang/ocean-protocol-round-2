853083deee8d94b5fccdcadf7a64a67751ce0d49
xjonwang <xjonwang@gmail.com>
2022-12-11 18:56:32 -0500

    added price pred

--- scripts/time_based_analysis.py
+++ scripts/time_based_analysis.py
@@ -4,7 +4,7 @@ import numpy as np
 cpi_data = pd.read_csv("../ETH_DATA/cpi_eth_5_minute_price.csv")
 spot_price = input("Input the spot price: ")
 #print(cpi_data)
-my_dic = pd.DataFrame(columns=['t+12', 't+24', 't+25', 't+26', 't+27', 't+28', 't+29', 't+30', 't+31', 't+32', 't+33', 't+34', 't+35', 't+36', 't+48', 't+54'])
+df = pd.DataFrame(columns=['t+12', 't+24', 't+25', 't+26', 't+27', 't+28', 't+29', 't+30', 't+31', 't+32', 't+33', 't+34', 't+35', 't+36', 't+48', 't+54'])
 
 for i in range(0, len(cpi_data), 864):
     day = cpi_data.loc[i : i + 863]
@@ -14,12 +14,12 @@ for i in range(0, len(cpi_data), 864):
     times = [12, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 48, 54]
     for j in range(0, 16):
         temp.loc[0][j] = day.loc[i + 12 * times[j]]['close'] / t0;
-    my_dic = pd.concat([my_dic, temp])
+    df = pd.concat([df, temp])
 
-#print(my_dic)
-my_dic = my_dic.mul(int(spot_price))
-print(my_dic.mean())
+#print(df)
+df = df.mul(int(spot_price))
+print(df.mean())
 print()
-print(my_dic.median())
+print(df.median())
 print()
-print(my_dic.std())
+print(df.std())
